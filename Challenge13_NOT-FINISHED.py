5bb693b0b1655e24487f39d657832038a390e321
StyxXx1337 <kefin87@gmail.com>
2021-08-08 13:36:48 +0900

    Updated 13 and 15 desciption

--- Challenge13_NOT-FINISHED.py
+++ Challenge13_NOT-FINISHED.py
@@ -1,2 +1,7 @@
 # Given an integer k and a string s, find the length of the longest substring that contains at most k distinct characters.
 # For example, given s = "abcba" and k = 2, the longest substring with k distinct characters is "bcb".
+# s = "abbbbbbbcccccccccccdd" k = 3, result = "bbbbbbbcccccccccccdd"
+
+
+def longest_string_with_k_letters(st: str, k: int) -> str:
+    letters = set(st)
