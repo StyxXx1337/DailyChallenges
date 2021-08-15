da4825630e816eeca337371cd686f3e1cdff7b83
StyxXx1337 <kefin87@gmail.com>
2021-08-15 10:24:54 +0900

    reformated

--- Challenge24_NOT-FINISHED.py
+++ Challenge24_NOT-FINISHED.py
@@ -1,4 +1,5 @@
-# Implement locking in a binary tree. A binary tree node can be locked or unlocked only if all of its descendants or ancestors are not locked.
+# Implement locking in a binary tree. A binary tree node can be locked or unlocked
+# only if all of its descendants or ancestors are not locked.
 # Design a binary tree node class with the following methods:
 # •	is_locked, which returns whether the node is locked
 # •	lock, which attempts to lock the node. If it cannot be locked, then it should return false.
