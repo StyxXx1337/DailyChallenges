f8b2f56e2b2ab25dee21ad151065610263742afa
Styx1337 <kefin87@gmail.com>
2021-08-23 09:58:37 +0900

    Reformatted the Comments to fit

--- Challenge35_NOT-FINISHED.py
+++ Challenge35_NOT-FINISHED.py
@@ -3,4 +3,5 @@
 # segregate the values of the array so that all the Rs come first, the Gs come second, and the Bs come last.
 # You can only swap elements of the array.
 # Do this in linear time and in-place.
-# For example, given the array ['G', 'B', 'R', 'R', 'B', 'R', 'G'], it should become ['R', 'R', 'R', 'G', 'G', 'B', 'B'].
+# For example, given the array ['G', 'B', 'R', 'R', 'B', 'R', 'G'],
+# it should become ['R', 'R', 'R', 'G', 'G', 'B', 'B'].
